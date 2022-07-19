62baa32fe5f9eb30092b325eb5ea960cad8d27f3
khaderman2000 <108017541+khaderman2000@users.noreply.github.com>
2022-07-19 21:29:23 +0300

    API task

---
+++ WebApplication2/WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using WebApplication2.Repo;
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet ]
+        public ActionResult<List<User>> GetAll()
+        {
+            return UserRepo.GetAll();
+        }
+        [HttpGet]
+        public ActionResult<User> Get(int id)
+        {
+            return UserRepo.Get(id);
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            UserRepo.Add(user);
+            return Ok();
+        }
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var user = UserRepo.Get(id);
+            if (user == null)
+                return NotFound();
+            UserRepo.Delete(id);
+            return Ok();
+        }
+        [HttpPut]
+        public ActionResult Update(int id, User user)
+        {
+            UserRepo.Update(id, user);
+            return Ok();
+        }
+
+    }
+}
---
+++ WebApplication2/WebApplication2/Repo/UserRepo.cs
@@ -0,0 +1,49 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Repo
+{
+
+    public class UserRepo
+    {
+        static List<User> users { get; set; }
+        static UserRepo()
+        {
+            users = new List<User>()
+            {
+                new User() { Id=1, FirstName= "khader", LastName="Mansour" },
+                new User() { Id=2, FirstName= "ala", LastName="sawadeh"},
+                new User() { Id=3, FirstName= "yaser", LastName="admeh"},
+                new User() { Id=4, FirstName= "bassam", LastName="kosa"},
+                new User() { Id=5, FirstName= "abed", LastName="fahed"},
+            };
+
+        }
+        public static List<User> GetAll()
+        {
+            return users;
+        }
+        public static User Get(int id)
+        {
+            return users.FirstOrDefault(users => users.Id == id);
+        }
+        public static void Add(User user)
+        {
+            users.Add(user);
+        }
+        public static void Delete(int id)
+        {
+            var user = Get(id);
+            if (user != null)
+                users.Remove(user);
+        }
+        public static void Update(int id, User user)
+        {
+            var index = users.FindIndex(u => u.Id == id);
+            if (index == -1)
+                return;
+            users[index] = user;
+
+        }
+
+    }
+}
