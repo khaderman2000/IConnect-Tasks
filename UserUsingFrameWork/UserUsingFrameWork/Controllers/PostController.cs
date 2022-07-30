6c337d93f7e38b36784152a4b521653f1972c929
khaderman2000 <108017541+khaderman2000@users.noreply.github.com>
2022-07-30 23:22:13 +0300

    middleware

--- UserUsingFrameWork/UserUsingFrameWork/Controllers/PostController.cs
+++ UserUsingFrameWork/UserUsingFrameWork/Controllers/PostController.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using UserUsingFrameWork.Models;
 using UserUsingFrameWork.Services;
-using UserUsingFrameWork.Fillters;
 
 namespace UserUsingFrameWork.Controllers
 {
